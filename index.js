6b5b2770ec57a9305ea5f845423e6cc30621a17a
Vlad Sorokin <sorokinvlad2077@gmail.com>
2021-08-31 22:20:51 -0500

    my first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function initApplication() {
+  let bankAccount1 = 5;
+  let bankAccount2 = 10;
+  let balance = addTenLines(bankAccount1, bankAccount2);
+  console.log('balance is: ' + balance);
+};
+
+function addTenLines(input1, input2) {
+  console.log('BEGIN addTenLines function');
+  let returnData;
+  for(let i = 0; i < 9; i++) {
+    
+    input1 = input1 + input2;
+    console.log('input1 is: ' + input1);
+
+  }
+  returnData = input1;
+  console.log('END addTenLines function');
+  return returnData;
+};
+
+initApplication();
