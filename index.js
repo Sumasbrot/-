a570d83b68ab751ccc86763246b18243d517e624
Vlad Sorokin <sorokinvlad2077@gmail.com>
2021-08-31 22:35:43 -0500

    add a subtract function

--- index.js
+++ index.js
@@ -9,13 +9,18 @@ function addTenLines(input1, input2) {
   console.log('BEGIN addTenLines function');
   let returnData;
   for(let i = 0; i < 9; i++) {
-    
+
     input1 = input1 + input2;
     console.log('input1 is: ' + input1);
 
   }
   returnData = input1;
   console.log('END addTenLines function');
+  return returnData;
+};
+
+function subtract(input1, input2) {
+  let returnData = input1 - input2;
   return returnData;
 };
 
